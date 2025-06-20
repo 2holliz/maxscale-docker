9b643403c16d4e55904f6ec97548a4972758c721
2holliz <maalghuraibawi@student.rtc.edu>
2025-06-20 12:07:12 -0700

    Update Shard_query.py

--- Shard_query.py
+++ Shard_query.py
@@ -1,30 +1,40 @@
+# shard_query.py
+# Name: Mustafa Alghuraibawi
+# Email: aliayad20135@gmail.com
+# Date: 06/20/2025
+# Class: Database Sharding Project
+# Description: Connects to MaxScale ReadWriteSplit router and runs sample queries on the sharded database.
+
 import pymysql
 
-db = pymysql.connect(host="192.168.1.20", port=4000, user="maxuser", passwd="maxpwd")
+# Connect to MaxScale router (ReadWriteSplit-router on port 4006)
+db = pymysql.connect(
+    host="127.0.0.1",  # localhost
+    port=4006,         # MaxScale router port
+    user="maxuser",
+    passwd="maxpwd",
+    database="zipcodes_one"  # You can choose one logical DB or omit to query fully qualified names
+)
+
 cursor = db.cursor()
 
-print('The last 10 rows of zipcodes_one are:')
-cursor.execute("SELECT * FROM zipcodes_one.zipcodes_one LIMIT 9990,10;")
-results = cursor.fetchall()
-for result in results:
-    print(result)
-
-print('The first 10 rows of zipcodes_tow are:')
-cursor.execute("SELECT * FROM zipcodes_two.zipcodes_two LIMIT 10")
-results = cursor.fetchall()
-for result in results:
-    print(result)
-
-print('The largest zipcode number in zipcodes_one is:')
-cursor = db.cursor()
+print("Largest zipcode in zipcodes_one:")
 cursor.execute("SELECT Zipcode FROM zipcodes_one.zipcodes_one ORDER BY Zipcode DESC LIMIT 1;")
-results = cursor.fetchall()
-for result in results:
-    print(result)
+print(cursor.fetchone())
 
-print('The smallest zipcode number in zipcodes_two is:')
-cursor = db.cursor()
-cursor.execute("SELECT Zipcode FROM zipcodes_two.zipcodes_two ORDER BY Zipcode ASC LIMIT 1")
-results = cursor.fetchall()
-for result in results:
-    print(result)
+print("\nAll zipcodes where state=KY:")
+cursor.execute("SELECT Zipcode FROM zipcodes_one.zipcodes_one WHERE State='KY';")
+for row in cursor.fetchall():
+    print(row)
+
+print("\nAll zipcodes between 40000 and 41000:")
+cursor.execute("SELECT Zipcode FROM zipcodes_one.zipcodes_one WHERE Zipcode BETWEEN 40000 AND 41000;")
+for row in cursor.fetchall():
+    print(row)
+
+print("\nTotalWages where state=PA:")
+cursor.execute("SELECT TotalWages FROM zipcodes_one.zipcodes_one WHERE State='PA';")
+for row in cursor.fetchall():
+    print(row)
+
+db.close()
